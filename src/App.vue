faae767e6a68a5f7886ec60f2b48c8e25e0929f5
Abdurrahman Seyidoğlu <abdurrahmanseyidoglu@gmail.com>
2024-08-24 19:43:25 +0300

    Add AppFooter

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,13 @@
   <main>
     <QuestionsArea :max-ranked-items="3" />
   </main>
+  <footer>
+    <AppFooter />
+  </footer>
 </template>
 
 <script setup lang="ts">
+import AppFooter from './components/AppFooter/AppFooter.vue'
 import AppNavbar from './components/AppNavbar/AppNavbar.vue'
 import QuestionsArea from './components/QuestionsArea/QuestionsArea.vue'
 </script>
---
+++ src/components/AppFooter/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="footer">
+    <a href="https://www.centiment.co/" target="_blank" class="footer__link">
+      <IconSecondaryLogo />
+      <p>Powered by Centiment</p>
+    </a>
+  </div>
+</template>
+<script setup lang="ts">
+import IconSecondaryLogo from '../icons/IconSecondaryLogo.vue'
+</script>
+<style lang="scss">
+@import './AppFooter.scss';
+</style>
